2b542d430eb4aaf86cf98775a800143c5267033d
unknown <moustikitos@gmail.com>
2020-09-09 20:30:42 +0200

    pyrex install finished

--- bash/pyrex-bin-install.sh
+++ bash/pyrex-bin-install.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-VENVDIR="$HOME/.local/share/pyrex/venv"
+VENVDIR=${HOME}/.local/share/pyrex/venv
 RAWGIT="https://raw.githubusercontent.com/Moustikitos/ark-hardener/master"
-IS_PY64="$(python -c 'import sys;print(sys.maxsize==2**64//2-1)')"
+TARGET="$(which python3)"
 
 clear
-
-echo
 echo installing system dependencies
 echo ==============================
-sudo apt-get -qq install ipset iptables-persistent
-sudo apt-get -qq install python3 python3-dev python3-setuptools python3-pip
-sudo apt-get -qq install virtualenv
+sudo apt-get -q install ipset iptables-persistent net-tools
+# install python3 if not found
+if [ ! -f  $TARGET ]; then
+	sudo apt-get -q install python3 python3-dev python3-setuptools python3-pip
+fi
+
 echo "done"
 
 echo
 echo creating virtual environment
 echo ============================
+$TARGET -m pip install --user --upgrade pip
+$TARGET -m pip install --user virtualenv
 
 if [ -d $VENVDIR ]; then
-    read -p "remove previous virtual environement ? [y/N]> " r
-    case $r in
+    read -p "remove previous virtual environement ? [y/N]> " R
+    case $R in
     y) rm -rf $VENVDIR;;
     Y) rm -rf $VENVDIR;;
     *) echo -e "previous virtual environement keeped";;
@@ -28,9 +31,8 @@ if [ -d $VENVDIR ]; then
 fi
 
 if [ ! -d $VENVDIR ]; then
-    TARGET="$(which python3)"
     mkdir $VENVDIR -p
-    virtualenv -p $TARGET $VENVDIR -q
+    $TARGET -m venv $VENVDIR
 fi
 
 echo "done"
@@ -93,24 +95,33 @@ echo
 echo installing python dependencies
 echo ==============================
 . $VENVDIR/bin/activate
-wget -q ${RAWGIT}/requirements.txt -P ${HOME}
-pip install -r ${HOME}/requirements.txt -q
-rm ${HOME}/requirements.txt
-echo "done"
+# from there: $TARGET --> python
 
-PY3="$(python3 -V)"
+# install dependencies
+wget -q ${RAWGIT}/requirements.txt -P ${HOME}
+python -m pip install -r ${HOME}/requirements.txt
+rm ${HOME}/requirements.txt
+
+# choose and install the right binary
+PY3="$(python -V)"
 MINOR="${PY3[@]: 9:1}"
+IS_PY64="$(python -c 'import sys; print(sys.maxsize==2**64//2-1)')"
+
 if [ $IS_PY64 = 'True' ]; then
   MACHINE="x64"
 else
   MACHINE="x32"
 fi
-wget -q ${RAWGIT}/bin/pyrex.${MACHINE}.cpython-3${MINOR}.so -P ${HOME}/.local/share
-mv ${HOME}/.local/share/pyrex.${MACHINE}.cpython-3${MINOR}.so ${HOME}/.local/share/pyrex.so
-chmod 777 ${HOME}/.local/share/pyrex
 
+wget -q ${RAWGIT}/bin/pyrex.${MACHINE}.cpython-3${MINOR}.so -P ${HOME}/.local/share
+mv --force ${HOME}/.local/share/pyrex.${MACHINE}.cpython-3${MINOR}.so ${HOME}/.local/share/pyrex.so
+
+# initialize pyrex
 export PYTHONPATH=${HOME}/.local/share
+chmod 777 ${HOME}/.local/share/pyrex
 python -c "import pyrex; pyrex.getP2pPort()"
+
+echo "done"
 
 echo
 echo starting services
